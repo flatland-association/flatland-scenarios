aa6446eb81efb24190215df5decec1ef8342a5e0
Manuel Meyer <maenu.meyer@bluewin.ch>
2026-01-14 13:54:52 +0100

    added drawing tool and scenario generator

---
+++ scenario_generator/README.md
@@ -0,0 +1,11 @@
+# Scenario generator
+
+A tool to help create environments and generate scenarios.
+
+## Drawing Tool
+
+Draw grid-maps, define Lines and Schedules.
+
+## Generate scenario
+
+Use the script to multiply the initial schedules to create a complete schedule.
---
+++ scenario_generator/scenario_1/Flatland_import.py
@@ -0,0 +1,72 @@
+from flatland.envs.rail_env import RailEnv
+from flatland.envs.timetable_utils import Line, Timetable
+from flatland.core.transition_map import GridTransitionMap
+from flatland.core.grid.rail_env_grid import RailEnvTransitions
+from flatland.envs.rail_trainrun_data_structures import Waypoint
+
+import json
+import numpy as np
+
+
+def rail_generator_from_grid_map(grid_map, level_free_positions):
+    def rail_generator(*args, **kwargs):
+        return grid_map, {
+            "agents_hints": {"city_positions": {}},
+            "level_free_positions": level_free_positions,
+        }
+
+    return rail_generator
+
+def line_generator_from_line(line):
+    def line_generator(*args, **kwargs):
+        return line
+
+    return line_generator
+
+
+def timetable_generator_from_timetable(timetable):
+    def timetable_generator(*args, **kwargs):
+        return timetable
+
+    return timetable_generator
+
+
+with open('scenario_1_generated.json', 'r') as f:
+    data = json.load(f)
+
+width = data['gridDimensions']['cols']
+height = data['gridDimensions']['rows']
+
+number_of_agents = len(data['flatland line']['agent_positions'])
+
+agent_positions = [[tuple(coords[0]) for coords in positions] for positions in data['flatland line']['agent_positions']]
+agent_directions = data['flatland line']['agent_directions']
+agent_targets = [tuple(coords[0]) for coords in data['flatland line']['agent_targets']]
+
+line = Line(
+    agent_waypoints = {i: [[Waypoint(p, d) for p, d in zip(pa, da)] for pa, da in zip(pas, das)] + [[Waypoint(target, None)]] for i, (pas, das, target) in enumerate(zip(agent_positions, agent_directions, agent_targets))},
+    agent_speeds = data['flatland line']['agent_speeds'],
+)
+
+timetable = Timetable(
+    earliest_departures = data['flatland timetable']['earliest_departures'],
+    latest_arrivals = data['flatland timetable']['latest_arrivals'],
+    max_episode_steps = data['flatland timetable']['max_episode_steps']
+)
+
+grid = GridTransitionMap(width=width, height=height, transitions=RailEnvTransitions())
+grid.grid = np.array(data['grid'])
+
+level_free_positions = [tuple(item) for item in data['overpasses']]
+
+
+env = RailEnv(
+    width = width,
+    height = height,
+    number_of_agents = number_of_agents,
+    rail_generator = rail_generator_from_grid_map(grid, level_free_positions),
+    line_generator = line_generator_from_line(line),
+    timetable_generator = timetable_generator_from_timetable(timetable),
+)
+
+observation, info = env.reset()
