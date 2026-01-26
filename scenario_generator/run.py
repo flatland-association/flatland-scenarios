e5be19e41767bd79528a8e1752e86fe16d8dce9e
chenkins <chenkins44@gmail.com>
2026-01-26 21:44:59 +0100

    refactor: improve scenario generator's run script.

--- scenario_generator/run.py
+++ scenario_generator/run.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pathlib import Path
 
 import pytest
@@ -9,25 +10,28 @@ from flatland_baselines.deadlock_avoidance_heuristic.policy.deadlock_avoidance_p
 
 class DeadlockAvoidanceNoHeuristics(DeadLockAvoidancePolicy):
     def __init__(self):
-        super().__init__(count_num_opp_agents_towards_min_free_cell=False, use_switches_heuristic=False)
+        super().__init__(
+            count_num_opp_agents_towards_min_free_cell=True,
+            use_switches_heuristic=True,
+            use_entering_prevention=True,
+        )
 
 
-def main(scenario: str, data_dir: str):
+def main(scenario: str, data_dir: str = None, generate_movies: bool = False):
+    data_dir = f"./results_{scenario}_{datetime.now().strftime('%Y%m%d_%H%M%S')}"  #
     Path(data_dir).mkdir(exist_ok=False)
-    generate_trajectory_from_policy([
-        "--data-dir", data_dir,
-        "--ep-id", scenario,
-        "--env-path", f"./{scenario}/{scenario}.pkl",
-        "--policy-pkg", "scenario_generator.run", "--policy-cls", "DeadlockAvoidanceNoHeuristics",
-        "--obs-builder-pkg", "flatland_baselines.deadlock_avoidance_heuristic.observation.full_env_observation", "--obs-builder-cls", "FullEnvObservation",
-        "--callbacks-pkg", "flatland.callbacks.generate_movie_callbacks", "--callbacks-cls", "GenerateMovieCallbacks"
-    ])
-
-
-if __name__ == '__main__':
-    data_dir = "./results20260126_1911"  #
     with pytest.raises(SystemExit) as e_info:
-        main("scenario_1", data_dir)
+        args = [
+            "--data-dir", data_dir,
+            "--ep-id", scenario,
+            "--env-path", f"./{scenario}/{scenario}.pkl",
+            "--policy-pkg", "scenario_generator.run", "--policy-cls", "DeadlockAvoidanceNoHeuristics",
+            "--obs-builder-pkg", "flatland_baselines.deadlock_avoidance_heuristic.observation.full_env_observation", "--obs-builder-cls", "FullEnvObservation",
+
+        ]
+        if generate_movies:
+            args += ["--callbacks-pkg", "flatland.callbacks.generate_movie_callbacks", "--callbacks-cls", "GenerateMovieCallbacks"]
+        generate_trajectory_from_policy(args)
         assert e_info.value.code == 0
 
     all_actions, all_trains_positions, all_trains_arrived, all_trains_rewards_dones_infos, env_stats, agent_stats = data_frame_for_trajectories(
@@ -35,3 +39,10 @@ if __name__ == '__main__':
     print(f"normalized_reward={all_trains_arrived["normalized_reward"].sum()}")
     print(f"mean_normalized_reward={all_trains_arrived["normalized_reward"].mean()}")
     print(f"success_rate={all_trains_arrived["success_rate"].mean()}")
+
+
+if __name__ == '__main__':
+    main(
+        "scenario_2",
+        # generate_movies=True,
+    )
