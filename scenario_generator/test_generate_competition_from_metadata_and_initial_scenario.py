36dc69bca79dcbf06d6c8a4b2081b9e3511069e5
Manuel Meyer <maenu.meyer@bluewin.ch>
2026-03-15 16:50:28 +0100

    updated filename and test

--- scenario_generator/test_generate_competition_from_metadata_and_initial_scenario.py
+++ scenario_generator/test_generate_competition_from_metadata_and_initial_scenario.py
@@ -2,22 +2,22 @@ import json
 import tempfile
 from pathlib import Path
 
-from scenario_generator.gen_comp_from_metadata_and_initial_scenario import gen_comp_from_metadata_and_initial_scenario
+from scenario_generator.generate_competition_from_metadata_and_initial_scenario import generate_competition_from_metadata_and_initial_scenario
 
 
 def test_gen_comp_from_metadata_and_initial_scenario():
     with tempfile.TemporaryDirectory() as tmpdirname:
         tmpdir = Path(tmpdirname)
-        gen_comp_from_metadata_and_initial_scenario(
+        generate_competition_from_metadata_and_initial_scenario(
             initial_scenario_file_name='scenario_generator/scene_sample_initial.json',
             metadata_file_name='scenario_generator/metadata_template.json',
             create_pkl=True,
             output_folder=tmpdir
         )
         files = {str(f.relative_to(tmpdirname)) for f in tmpdir.rglob("**/*") if f.is_file()}
-        assert files == {"level_0/level_0_scenario_1.pkl", "level_0/level_0_scenario_1.json"}
+        assert files == {"competition/level_0/level_0_scenario_1.pkl", "competition/level_0/level_0_scenario_1.json"}
 
-        with (tmpdir / "level_0/level_0_scenario_1.json").open() as f:
+        with (tmpdir / "competition/level_0/level_0_scenario_1.json").open() as f:
             data = json.load(f)
             print(data)
 
