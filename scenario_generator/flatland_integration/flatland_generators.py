d5df9d4ee55c44e069e59c1dfbaf52f3171dce8f
chenkins <chenkins44@gmail.com>
2026-03-18 10:28:20 +0100

    refactor: add type hints.

--- scenario_generator/flatland_integration/flatland_generators.py
+++ scenario_generator/flatland_integration/flatland_generators.py
@@ -1,4 +1,10 @@
-def rail_generator_from_grid_map(grid_map, level_free_positions):
+from typing import List, Tuple
+
+from flatland.envs.rail_grid_transition_map import RailGridTransitionMap
+from flatland.envs.timetable_utils import Timetable, Line
+
+
+def rail_generator_from_grid_map(grid_map: RailGridTransitionMap, level_free_positions: List[Tuple[int, int]]):
     def rail_generator(*args, **kwargs):
         return grid_map, {
             "agents_hints": {"city_positions": {}},
@@ -8,14 +14,14 @@ def rail_generator_from_grid_map(grid_map, level_free_positions):
     return rail_generator
 
 
-def line_generator_from_line(line):
+def line_generator_from_line(line: Line):
     def line_generator(*args, **kwargs):
         return line
 
     return line_generator
 
 
-def timetable_generator_from_timetable(timetable):
+def timetable_generator_from_timetable(timetable: Timetable):
     def timetable_generator(*args, **kwargs):
         return timetable
 
