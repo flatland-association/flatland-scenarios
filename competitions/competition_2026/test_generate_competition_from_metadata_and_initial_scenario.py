9b3e0fa038394facd7ebe83897b919afed8be0d4
chenkins <chenkins44@gmail.com>
2026-04-23 08:04:38 +0200

    refactor: use IntermediateStopMalfunctionEffectsGenerator for departure malfunctions.

--- competitions/competition_2026/test_generate_competition_from_metadata_and_initial_scenario.py
+++ competitions/competition_2026/test_generate_competition_from_metadata_and_initial_scenario.py
@@ -5,6 +5,9 @@ from pathlib import Path
 
 from competitions.competition_2026.generate_competition_from_metadata_and_initial_scenario import get_scenes_from_timetable, \
     generate_competition_from_metadata_and_initial_scenario
+from flatland.core.effects_generator import MultiEffectsGeneratorWrapped
+from flatland.envs.malfunction_effects_generators import MalfunctionEffectsGenerator, IntermediateStopMalfunctionEffectsGenerator
+from flatland.envs.malfunction_generators import MalfunctionParameters
 from flatland.envs.persistence import RailEnvPersister
 
 
@@ -44,7 +47,15 @@ def test_generate_competition_from_metadata_and_initial_scenario():
             assert actual == expected
 
             env, _ = RailEnvPersister.load_new(f"{tmpdir}/level_0/level_0_scenario_1.pkl")
-            assert len(env.effects_generator.__getstate__()) == 2
+            assert isinstance(env.effects_generator, MultiEffectsGeneratorWrapped)
+            assert len(env.effects_generator.effects_generators) == 2
+
+            assert isinstance(env.effects_generator.effects_generators[0], IntermediateStopMalfunctionEffectsGenerator)
+            assert env.effects_generator.effects_generators[0]._malfunction_generator.MFP == MalfunctionParameters(0.1, 2, 5)
+            assert env.effects_generator.effects_generators[0]._earliest_condition == 0
+
+            assert isinstance(env.effects_generator.effects_generators[1], MalfunctionEffectsGenerator)
+            assert env.effects_generator.effects_generators[1].malfunction_generator.MFP == MalfunctionParameters(1 / 540, 20, 50)
 
 
 def test_generate_competition_from_metadata_and_initial_scenario_filter_levels():
