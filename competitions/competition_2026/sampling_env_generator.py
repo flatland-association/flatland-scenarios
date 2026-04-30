dc1cd386c7de8f2729016a14b488fa43d2ce5f6d
Manuel Meyer <maenu.meyer@bluewin.ch>
2026-04-30 22:34:25 +0200

    feat: add stations.pkl for sampling

--- competitions/competition_2026/sampling_env_generator.py
+++ competitions/competition_2026/sampling_env_generator.py
@@ -1,45 +1,36 @@
-import numpy as np
+import pickle
 
-from flatland.core.grid.grid4 import Grid4TransitionsEnum
 from flatland.envs.line_generators import sparse_line_generator
 from flatland.envs.rail_env import RailEnv
 from flatland.envs.timetable_generators import timetable_generator
 
 
 def sampling_env_gnerator(env: RailEnv):
-    height = env.height
-    width = env.width
-
-    stations = []
-    # 5 in parallel vertically -> E/W
-    for r in range(height - 5):
-        for c in range(width):
-            if np.count_nonzero(env.rail.grid[r:r + 5, c]) == 5:
-                stations.append(((r, c), Grid4TransitionsEnum.EAST))
-                stations.append(((r, c), Grid4TransitionsEnum.WEST))
-    # 5 in parallel horizontally -> N/S
-    for r in range(height):
-        for c in range(width):
-            if np.count_nonzero(env.rail.grid[r, c:c + 5]) == 5:
-                stations.append(((r, c), Grid4TransitionsEnum.NORTH))
-                stations.append(((r, c), Grid4TransitionsEnum.SOUTH))
 
     _previous_rail_generator = env.rail_generator
+
+    with open('stations.pkl', 'rb') as f:
+        data = pickle.load(f)
+
+    city_positions = data["city_positions"]
+    city_orientations = data["city_orientations"]
+    train_stations = data["train_stations"]
+    level_free_positions = data["level_free_positions"]
 
     def rail_generator(*args, **kwargs):
         rail, _ = _previous_rail_generator(*args, **kwargs)
         optionals = {'agents_hints':
             {
-                'city_positions': [pos for pos, _ in stations],
-                # single-station cities as a workaround
-                'train_stations': [[station] for station in stations],
-                'city_orientations': [d for _, d in stations],
+                'city_positions': city_positions,
+                'city_orientations': city_orientations,
+                'train_stations': train_stations,
             },
-            'level_free_positions': []
+            'level_free_positions': level_free_positions
         }
         return rail, optionals
 
     env.rail_generator = rail_generator
     env.line_generator = sparse_line_generator()
     env.timetable_generator = timetable_generator
+
     return env
