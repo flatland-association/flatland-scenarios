171f7e1443d24a9958e0bf5807dc859dabdfe72d
Manuel Meyer <maenu.meyer@bluewin.ch>
2026-05-04 12:01:48 +0200

    feat: added tes for line_length > 2

--- competitions/competition_2026/test_sampling_env_generator.py
+++ competitions/competition_2026/test_sampling_env_generator.py
@@ -1,11 +1,31 @@
 import numpy as np
 
-from competitions.competition_2026.sampling_env_generator import sampling_env_gnerator
+from competitions.competition_2026.sampling_env_generator import sampling_env_generator
 from flatland.envs.persistence import RailEnvPersister
 
 
 def test_sampling_env_generator() -> None:
-    sampling_env = sampling_env_gnerator(RailEnvPersister.load_new("level_0_scenario_1.pkl", load_from_package="competitions.competition_2026.level_0")[0])
+    sampling_env = sampling_env_generator(RailEnvPersister.load_new("level_0_scenario_1.pkl", load_from_package="competitions.competition_2026.level_0")[0])
+    env, _ = RailEnvPersister.load_new("level_0_scenario_1.pkl", load_from_package="competitions.competition_2026.level_0")
+
+    assert np.array_equal(env.rail.grid, sampling_env.rail.grid)
+    assert env.agents == sampling_env.agents
+
+    # verify sampling env produces new randomly sampled lines on the same grid
+    sampling_env.reset()
+    assert np.array_equal(env.rail.grid, sampling_env.rail.grid)
+    assert len(env.agents) == len(sampling_env.agents)
+    assert env.agents != sampling_env.agents
+
+    # sanity check: reset on env loaded from file always produces same (see https://github.com/flatland-association/flatland-rl/pull/408)
+    env_raw, _ = RailEnvPersister.load_new("level_0_scenario_1.pkl", load_from_package="competitions.competition_2026.level_0")
+    env.reset()
+    assert np.array_equal(env.rail.grid, env_raw.rail.grid)
+    assert env.agents == env_raw.agents
+
+
+def test_sampling_env_generator_line_length_3() -> None:
+    sampling_env = sampling_env_generator(RailEnvPersister.load_new("level_0_scenario_1.pkl", load_from_package="competitions.competition_2026.level_0")[0], line_length=3)
     env, _ = RailEnvPersister.load_new("level_0_scenario_1.pkl", load_from_package="competitions.competition_2026.level_0")
 
     assert np.array_equal(env.rail.grid, sampling_env.rail.grid)
